9b829dd6c5d7b556dd59f8944390b6f12c6dddb1
Vipin Yadav <yadavipin57@gmail.com>
2024-12-16 20:35:56 +0530

    .env file made and api key secured

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPVuoS9Xy-4mAtZJqTVA1a5pwOIIQCzjY",
+  apiKey: import.meta.env.VITE_GOOGLE_FIREBASE_KEY,
   authDomain: "ssb-prep-545.firebaseapp.com",
   projectId: "ssb-prep-545",
   storageBucket: "ssb-prep-545.firebasestorage.app",
